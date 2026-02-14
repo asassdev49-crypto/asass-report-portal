e15ccf75c02d2c34a4472b09042715050f7e4d3b
asassdev49-crypto <asassdev49@gmail.com>
2026-02-14 15:19:29 +0000

    Update supabase.js
    supabase client

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,10 @@
-// js/supabase.js
-const SUPABASE_URL = "https://zuahdikwqlcsuyrfrjeg.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1YWhkaWt3cWxjc3V5cmZyamVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NDMzMjIsImV4cCI6MjA4NjExOTMyMn0.OnJtcrbG9O1TaEvqLnDnE1kIMZtqNUAy_AwFiPFrObg";
 
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+
+
+
+
+const { createClient } = supabase;
+
+const SUPABASE_URL = "https://zuahdikwqlcsuyrfrjeg.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1YWhkaWt3cWxjc3V5cmZyamVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NDMzMjIsImV4cCI6MjA4NjExOTMyMn0.OnJtcrbG9O1TaEvqLnDnE1kIMZtqNUAy_AwFiPFrObg";
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
